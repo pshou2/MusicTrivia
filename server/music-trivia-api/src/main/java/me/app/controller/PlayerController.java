794fa3d5afd1b4223ec7b58faf257d673441d9e5
pshou2 <89333546+pshou2@users.noreply.github.com>
2023-09-27 17:14:19 -0400

    finished testing findall and findbyid in player controller

--- server/music-trivia-api/src/main/java/me/app/controller/PlayerController.java
+++ server/music-trivia-api/src/main/java/me/app/controller/PlayerController.java
@@ -2,10 +2,9 @@ package me.app.controller;
 
 import me.app.domain.PlayerService;
 import me.app.models.Player;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +21,14 @@ public class PlayerController {
     @GetMapping
     public List<Player> findAll(){
         return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Player> findById(@PathVariable int id){
+        Player player = service.findById(id);
+        if (player == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(player, HttpStatus.OK);
     }
 }

93900ca19c4373efa34d1a62902a85d0b2ca52d1
pshou2 <89333546+pshou2@users.noreply.github.com>
2023-09-27 18:19:46 -0400

    finished highscore controller methods and tested them

--- server/music-trivia-api/src/main/java/me/app/controller/HighScoreController.java
+++ server/music-trivia-api/src/main/java/me/app/controller/HighScoreController.java
@@ -1,6 +1,7 @@
 package me.app.controller;
 
 import me.app.domain.HighScoreService;
+import me.app.domain.Result;
 import me.app.models.HighScore;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -33,5 +34,34 @@ public class HighScoreController {
     }
 
     @PostMapping
-    
+    public ResponseEntity<Object> add(@RequestBody HighScore highScore){
+        Result<HighScore> result = service.add(highScore);
+        if(result.isSuccess()){
+            return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
+        }
+
+        return ErrorResponse.build(result);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> update(@PathVariable int id, @RequestBody HighScore highScore){
+        if (id != highScore.getHighScoresId()){
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+
+        Result<HighScore> result = service.update(highScore);
+        if(result.isSuccess()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return ErrorResponse.build(result);
+    }
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<Object> deleteById(@PathVariable int id){
+        Result<HighScore> result = service.deleteById(id);
+        if(result.isSuccess()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return ErrorResponse.build(result);
+    }
 }

13f7e0b1e5c5ca3e907dec636b31f8d75b6040f0
pshou2 <89333546+pshou2@users.noreply.github.com>
2023-10-03 21:50:26 -0400

    added form to CreateGamer Component with styling

--- client/music-trivia-react/src/CreateGamer.js
+++ client/music-trivia-react/src/CreateGamer.js
@@ -1,9 +1,37 @@
-
+import { useState } from "react";
 
 function CreateGamer(){
+    const [gamer, setGamer] = useState({
+        gamer_tag: '',
+        tagline: ''
+    });
+
+    //handle methods
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    };
+
     return (
         <>
-            <p>This is create gamer</p>
+            <div className="container-fluid">
+                <h1>CREATE YOUR GAMER</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="gamer_tag" className="form-label">Enter your Name: </label>
+                        <input
+                        type="text"
+                        className="form-control"
+                        id="gamer_tag"
+                        name="gamer_tag"
+                        placeholder="GAMER420"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="tagline" className="form-label">Tagline: </label>
+                        <textarea id="tagline" name="tagline"></textarea>
+                    </div>
+                </form>
+            </div>
         </>
     );
 }
